9a92071fb998ffcce591fbf70cb1561b41c89962
bibincatchme <42738880+bibincatchme@users.noreply.github.com>
2019-05-09 15:27:30 +0530

    Update Install_wkhtmltopdf_on_CentOS7.sh
    updated commit

--- Install_wkhtmltopdf_on_CentOS7.sh
+++ Install_wkhtmltopdf_on_CentOS7.sh
@@ -1,37 +1,36 @@
+#!/bin/bash
+#Install dependencies (if needed)
+yum install -y libpng
+yum install -y libjpeg
+yum install -y openssl 
+yum install -y icu
+yum install -y libX11
+yum install -y libXext
+yum install -y libXrender
+yum install -y xorg-x11-fonts-Type1
+yum install -y xorg-x11-fonts-75dpi
+yum install xz
 
-# Install dependencies (if needed)
-$yum install -y libpng
-$yum install -y libjpeg
-$yum install -y openssl
-$yum install -y icu
-$yum install -y libX11
-$yum install -y libXext
-$yum install -y libXrender
-$yum install -y xorg-x11-fonts-Type1
-$yum install -y xorg-x11-fonts-75dpi
-$ yum install xz
+#Get latest version of wkhtmltopdf (replace version number if needed)
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 
-# Get latest version of wkhtmltopdf (replace version number if needed)
-$ wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-
-# Untar and move wkhtmltox
-$ unxz wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-$ tar -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar
-$mv wkhtmltox/bin/* /usr/local/bin/
+#Untar and move wkhtmltox
+unxz wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+tar -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar
+mv wkhtmltox/bin/* /usr/local/bin/
 
 
-# Add wkhtmltopdf to .bash_profile (optional)
+#Add wkhtmltopdf to .bash_profile (optional)
 #nano .bash_profile
 # Add the following line at the end of the .bash_profile file
 # export PATH=/usr/bin/wkhtmltox/bin:$PATH
 
 # And don't forget..
-$rm -rf wkhtmltox
-$rm -f wkhtmltox-0.12.4_linux-generic-amd64.tar
+rm wkhtmltox
+rm wkhtmltox-0.12.4_linux-generic-amd64.tar
 
 # Finally enjoy!
-$ wkhtmltopdf http://www.google.com google.pdf
-
+wkhtmltopdf http://www.google.com google.pdf
 
 #Using RPM file
 #Download the wkhtmltopdf RPM file
